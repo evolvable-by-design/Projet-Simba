30c75a3a32ae565b62239f33976229755b1b52e7
safdab <daoudabarry9@gmail.com>
2019-05-28 12:48:55 +0200

    classe pad terminee

--- api/src/main/java/com/project/doodle/models/PadFeature.java
+++ api/src/main/java/com/project/doodle/models/PadFeature.java
@@ -2,8 +2,6 @@ package com.project.doodle.models;
 
 import net.gjerull.etherpad.client.EPLiteClient;
 
-import java.util.List;
-import java.util.Map;
 
 public class PadFeature {
 
@@ -12,25 +10,37 @@ public class PadFeature {
         String padId;
         String padUrl;
 
+        private static final String apikey = "00d4bfe69d0cb2bb7c3a3491500d31a89d39335a4c18cc89670b3c6f849c901c";
+
         public PadFeature(String padUrl, String apikey, String padId) {
             client = new EPLiteClient(padUrl, apikey);
             this.padId = padId;
             this.padUrl = padUrl;
             client.createPad(padId);
-
-            checkPad();
         }
 
+
+        public void createAuthor(String auth){
+            this.client.createAuthor(auth);
+        }
+
+
         public static void main(String[] args) {
-            PadFeature pad = new PadFeature("http://localhost:9001/", "00d4bfe69d0cb2bb7c3a3491500d31a89d39335a4c18cc89670b3c6f849c901c", "testPad1 ");
+            PadFeature pad = new PadFeature("http://localhost:9001/", apikey, "id");
             pad.addUser("Anne");
             pad.addUser("moi");
             pad.addUser("toi");
+            System.out.println( pad.getPadUrl());
+
+        }
+
+        public String getPadUrl(){
+            return this.padUrl + "p/"+ this.padId;
         }
 
         public void addUser(String user) {
             String str = client.getText(padId).get("text").toString();
-            final String substring = "Liste des participants:\n";
+            final String substring = "Give your ideas here :\n";
             int index = str.indexOf(substring);
             if (index == -1) {
                 str = substring;
@@ -43,17 +53,5 @@ public class PadFeature {
             client.setText(padId, str);
         }
 
-        public void setTitle(String title) {
-            //TODO
-        }
 
-        private void checkPad() {
-            Map result = client.listAllPads();
-            List padIds = (List) result.get("padIDs");
-            boolean exist = padIds.contains(padId);
-            if(!exist) {
-                client.createPad(padId);
-            }
-
-        }
 }
