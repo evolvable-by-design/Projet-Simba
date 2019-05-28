cbff7cb1820ea88a0a4293bf26113f5d32f4ffa6
Kevin Kheav <kevin.kheav@hotmail.fr>
2019-05-28 13:08:17 +0200

    Moving ExportResource to new packege features and adding jxl dependecy

--- api/src/main/java/com/project/doodle/features/ExportResource.java
+++ api/src/main/java/com/project/doodle/features/ExportResource.java
@@ -1,4 +1,4 @@
-package com.project.doodle.controllers;
+package com.project.doodle.features;
 
 import com.project.doodle.models.Poll;
 import com.project.doodle.repositories.ChoiceRepository;
@@ -19,8 +19,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import jxl.Workbook;
+import jxl.write.*;
+import jxl.write.Number;
+
+import java.io.File;
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.Optional;
 
 
 @RestController
@@ -34,20 +42,84 @@ public class ExportResource {
     @Autowired
     private UserRepository userRepository;
 
-    private static final String APPLICATION_PDF = "application/pdf";
     private static final String APPLICATION_EXCEL = "application/vnd.ms-excel";
+    private static final String APPLICATION_PDF = "application/pdf";
+    private static final String EXCEL_FILE_LOCATION = "./generatedFiles/MyFirstExcel.xls";
 
-    @RequestMapping(value = "/polls/{idPoll}/print", method = RequestMethod.GET, produces = APPLICATION_PDF)
+
+    @RequestMapping(value = "/polls/{slug}/results", method = RequestMethod.GET, produces = APPLICATION_EXCEL)
+    public @ResponseBody HttpEntity<byte[]> downloadResultsExcel(@PathVariable String slug) throws IOException {
+        createExcelFile(slug);
+        String filePath = "./Test.xlsx";
+        return getHttpEntityToDownload(filePath,"vnd.ms-excel");
+    }
+    @RequestMapping(value = "/polls/{slug}/print", method = RequestMethod.GET, produces = APPLICATION_PDF)
     public @ResponseBody HttpEntity<byte[]> downloadResultsPdf() throws IOException {
         String filePath = "./Test.pdf";
         return getHttpEntityToDownload(filePath,"pdf");
     }
 
-    @RequestMapping(value = "/polls/{idPoll}/results", method = RequestMethod.GET, produces = APPLICATION_EXCEL)
-    public @ResponseBody HttpEntity<byte[]> downloadResultsExcel() throws IOException {
-        String filePath = "./Test.xlsx";
-        return getHttpEntityToDownload(filePath,"vnd.ms-excel");
-    }
+   private void createExcelFile(String slug){
+
+       Optional<Poll> poll = pollRepository.findBySlug(slug);
+       if (!poll.isPresent()) {
+           return ;
+       }
+       // Create an Excel file
+       WritableWorkbook myFirstWbook = null;
+       try {
+           // Create an Excel file in the file location
+           myFirstWbook = Workbook.createWorkbook(new File(EXCEL_FILE_LOCATION));
+           // Create an Excel sheet
+           WritableSheet myFirstSheet = myFirstWbook.createSheet("SONDAGE", 0);
+
+           WritableCellFormat cFormat = new WritableCellFormat();
+           WritableFont font = new WritableFont(WritableFont.ARIAL, 16, WritableFont.BOLD);
+           cFormat.setFont(font);
+           Label label;
+           Number number;
+
+
+           label = new Label(0, 0, "Sondage \""+"title"+"\"", cFormat);
+           myFirstSheet.addCell(label);
+           number = new Number(0, 1, 1);
+           myFirstSheet.addCell(number);
+
+           label = new Label(1, 0, "Result", cFormat);
+           myFirstSheet.addCell(label);
+           label = new Label(1, 1, "Passed");
+           myFirstSheet.addCell(label);
+
+           number = new Number(0, 2, 2);
+           myFirstSheet.addCell(number);
+
+           label = new Label(1, 2, "Passed 2");
+           myFirstSheet.addCell(label);
+
+           // Writes out the data held in this workbook in Excel format
+           myFirstWbook.write();
+
+       } catch (WriteException e) {
+           e.printStackTrace();
+       } catch (IOException e) {
+           e.printStackTrace();
+       } finally {
+
+           if (myFirstWbook != null) {
+               try {
+                   myFirstWbook.close();
+               } catch (IOException e) {
+                   e.printStackTrace();
+               } catch (WriteException e) {
+                   e.printStackTrace();
+               }
+           }
+
+
+       }
+
+
+   }
 
 
 
